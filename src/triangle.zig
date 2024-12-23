bbf6550d90ced54f87ea704d824b4740072f911c
Stefan Eletzhofer <stefan.eletzhofer@nexiles.com>
2024-12-23 17:03:14 +0100

    Switch to sokol, multiple build targets, triangle example

---
+++ src/triangle.zig
@@ -0,0 +1,68 @@
+//------------------------------------------------------------------------------
+//  triangle.zig
+//
+//  Vertex buffer, shader, pipeline state object.
+//------------------------------------------------------------------------------
+const sokol = @import("sokol");
+const slog = sokol.log;
+const sg = sokol.gfx;
+const sapp = sokol.app;
+const sglue = sokol.glue;
+const shd = @import("shaders/triangle.glsl.zig");
+
+const state = struct {
+    var bind: sg.Bindings = .{};
+    var pip: sg.Pipeline = .{};
+};
+
+export fn init() void {
+    sg.setup(.{
+        .environment = sglue.environment(),
+        .logger = .{ .func = slog.func },
+    });
+
+    // create vertex buffer with triangle vertices
+    state.bind.vertex_buffers[0] = sg.makeBuffer(.{
+        .data = sg.asRange(&[_]f32{
+            // positions         colors
+            0.0,  0.5,  0.5, 1.0, 0.0, 0.0, 1.0,
+            0.5,  -0.5, 0.5, 0.0, 1.0, 0.0, 1.0,
+            -0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 1.0,
+        }),
+    });
+
+    // create a shader and pipeline object
+    var pip_desc: sg.PipelineDesc = .{
+        .shader = sg.makeShader(shd.triangleShaderDesc(sg.queryBackend())),
+    };
+    pip_desc.layout.attrs[shd.ATTR_triangle_position].format = .FLOAT3;
+    pip_desc.layout.attrs[shd.ATTR_triangle_color0].format = .FLOAT4;
+    state.pip = sg.makePipeline(pip_desc);
+}
+
+export fn frame() void {
+    // default pass-action clears to grey
+    sg.beginPass(.{ .swapchain = sglue.swapchain() });
+    sg.applyPipeline(state.pip);
+    sg.applyBindings(state.bind);
+    sg.draw(0, 3, 1);
+    sg.endPass();
+    sg.commit();
+}
+
+export fn cleanup() void {
+    sg.shutdown();
+}
+
+pub fn main() void {
+    sapp.run(.{
+        .init_cb = init,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+        .width = 640,
+        .height = 480,
+        .icon = .{ .sokol_default = true },
+        .window_title = "triangle.zig",
+        .logger = .{ .func = slog.func },
+    });
+}
